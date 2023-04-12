3dc46f33f5dd66e31fc6c7801af82aea61239912
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-04-12 10:32:11 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "wispy-bread-39691",
-  url: "https://wispy-bread-39691.botics.co",
-  api: "https://wispy-bread-39691.botics.co/api/v1"
+  "name": "wispy-bread-39691",
+  "url": "https://wispy-bread-39691.botics.co",
+  "api": "https://wispy-bread-39691.botics.co/api/v1",
+  "initialRoute": "Splash"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
